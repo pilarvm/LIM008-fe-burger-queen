1d6b309418ef616ea157d40eb2fa4da20206605c
pjvarillas <pjvarillas@gmail.com>
2019-03-28 18:43:37 -0500

    se creo acceso a firebase y carpetas de react

---
+++ build/precache-manifest.41f7196045958986a46243478f9d9207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b36ccdc784acb104a02d",
+    "url": "/static/js/main.dabf6937.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b36ccdc784acb104a02d",
+    "url": "/static/css/main.8c1b20bc.chunk.css"
+  },
+  {
+    "revision": "321a3ad02d588d65640ed9b1f76b7b0f",
+    "url": "/index.html"
+  }
+];
