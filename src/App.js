a578edb9c48f38e27f909450daefd2e5d69b3b3c
pjvarillas <pjvarillas@gmail.com>
2019-04-04 18:25:25 -0500

    agregando cards desde un json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navigation from './components/Navigation';
+import { todos } from './menu.json';
+console.log(todos);
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
   render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+            </div>
+            <div className="card-body">
+              <p>{todo.descripcion}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
     return (
       <div className="App">
+
+        <Navigation titulo="hola" />
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Task
+          </a>
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+
       </div>
     );
   }
